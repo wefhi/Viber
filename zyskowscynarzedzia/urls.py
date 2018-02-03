b645048baf7a4e5323c4c8c58788210e55255ac0
adrian <zyskow.ad@gmail.com>
2018-02-03 22:57:53 +0100

    added index page

--- zyskowscynarzedzia/urls.py
+++ zyskowscynarzedzia/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^music/', include('music.urls')),
+    url(r'', include('music.urls')),
 ]
 
 
